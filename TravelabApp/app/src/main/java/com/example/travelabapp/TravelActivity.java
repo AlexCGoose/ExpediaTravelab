65948a773e2a865f13bca1a5c4df0fdb459538fc
Piraveenan Kirupakaran <piraveenankirupakaran@Piraveenans-MacBook-Pro.local>
2019-11-16 08:04:35 +0000

    date picker

--- TravelabApp/app/src/main/java/com/example/travelabapp/TravelActivity.java
+++ TravelabApp/app/src/main/java/com/example/travelabapp/TravelActivity.java
@@ -1,35 +1,50 @@
 package com.example.travelabapp;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import android.app.DatePickerDialog;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Calendar;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class TravelActivity extends AppCompatActivity implements View.OnClickListener{
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Iterator;
+
+public class TravelActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editTextFlightFrom, editTextFlightTo, editTextDepartDate, editTextReturnDate;
     private Button buttonSearch;
-    private TextView textView;
+
     private Button returnDate;
     private Button departDate;
     private TextView returnDate1;
     private TextView departDate1;
+    private String storeDate1;
+    private String storeDate2;
     Calendar c;
     DatePickerDialog dpd;
-
 
 
     @Override
@@ -42,11 +57,12 @@ public class TravelActivity extends AppCompatActivity implements View.OnClickLis
         departDate = (Button) findViewById(R.id.departDate);
         returnDate = (Button) findViewById(R.id.returnDate);
         buttonSearch = (Button) findViewById(R.id.buttonSearch);
-        textView = (TextView) findViewById(R.id.departDate1);
+        //textView = (TextView) findViewById(R.id.departDate1);
         departDate1 = (TextView) findViewById(R.id.departDate1);
         returnDate1 = (TextView) findViewById(R.id.returnDate1);
 
 
+        buttonSearch.setOnClickListener(this);
         returnDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,9 +74,10 @@ public class TravelActivity extends AppCompatActivity implements View.OnClickLis
                 dpd = new DatePickerDialog(TravelActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int mYear, int mMonth, int mDayOfMonth) {
-                        returnDate1.setText("Return Date: "+ mDayOfMonth + "/" + (mMonth + 1) + "/" + mYear);
+                        storeDate1 = mDayOfMonth + "/" + (mMonth + 1) + "/" + mYear;
+                        returnDate1.setText("Depart Date: " + storeDate1);
                     }
-                }, day, month , year);
+                }, day, month, year);
                 dpd.show();
 
             }
@@ -77,9 +94,10 @@ public class TravelActivity extends AppCompatActivity implements View.OnClickLis
                 dpd = new DatePickerDialog(TravelActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int mYear, int mMonth, int mDayOfMonth) {
-                        departDate1.setText("Depart Date: "+ mDayOfMonth + "/" + (mMonth + 1) + "/" + mYear);
+                        storeDate2 = mDayOfMonth + "/" + (mMonth + 1) + "/" + mYear;
+                        departDate1.setText("Depart Date: " + storeDate2);
                     }
-                }, day, month , year);
+                }, day, month, year);
                 dpd.show();
 
             }
@@ -87,33 +105,22 @@ public class TravelActivity extends AppCompatActivity implements View.OnClickLis
 
     }
 
+
     @Override
+
     public void onClick(View view) {
-        if(view == buttonSearch) {
-            //TODO - Must change params to right format e.g. london_gb and 16/11/2019
-//            String apiUrl = "https://api.skypicker.com/flights?fly_from="+editTextFlightFrom+"&fly_to="+editTextFlightTo+"&date_from="+editTextDepartDate+"&date_to="+editTextReturnDate+"&partner=picky&vehicle_type=aircraft";
-            String apiUrl = "https://api.skypicker.com/flights?fly_from=london_gb&fly_to=cardiff_gb&date_from=16/11/2019&date_to=17/11/2019&partner=picky&vehicle_type=aircraft";
-            RequestQueue queue = Volley.newRequestQueue(this);
+        if (view == buttonSearch) {
 
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, apiUrl,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            // Display the first 500 characters of the response string.
+            String apiUrl = "https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=17/11/2019&dateTo=18/11/2019&partner=picky";
+            URL obj = new URL(GET_URL);
+            Gson gson = new Gson();
 
-                            textView.setText("Response is: "+ response.substring(0,500));
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    textView.setText("That didn't work!");
-                }
-            });
-
-// Add the request to the RequestQueue.
-            queue.add(stringRequest);
+            String json = gson.toJson(apiUrl);
+            Toast.makeText(this, json, Toast.LENGTH_LONG).show();
 
         }
 
     }
+
 }
+

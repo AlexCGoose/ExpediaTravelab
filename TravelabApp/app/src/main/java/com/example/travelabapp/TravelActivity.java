4ffd860e9b438f291b04e3ce118d4ec6ee19630f
Piraveenan Kirupakaran <piraveenankirupakaran@Piraveenans-MacBook-Pro.local>
2019-11-16 02:21:08 +0000

    changes to the travel page

--- TravelabApp/app/src/main/java/com/example/travelabapp/TravelActivity.java
+++ TravelabApp/app/src/main/java/com/example/travelabapp/TravelActivity.java
@@ -6,19 +6,30 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class TravelActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText editTextFlightFrom, editTextFlightTo, editTextDepartDate, editTextReturnDate;
     private Button buttonSearch;
     private TextView textView;
+    private Button returnDate;
+    private Button departDate;
+    private TextView returnDate1;
+    private TextView departDate1;
+    Calendar c;
+    DatePickerDialog dpd;
+
 
 
     @Override
@@ -28,12 +39,51 @@ public class TravelActivity extends AppCompatActivity implements View.OnClickLis
 
         editTextFlightFrom = (EditText) findViewById(R.id.editTextFlightFrom);
         editTextFlightTo = (EditText) findViewById(R.id.editTextFlightTo);
-        editTextDepartDate = (EditText) findViewById(R.id.editTextDepartDate);
-        editTextReturnDate = (EditText) findViewById(R.id.editTextReturnDate);
+        departDate = (Button) findViewById(R.id.departDate);
+        returnDate = (Button) findViewById(R.id.returnDate);
         buttonSearch = (Button) findViewById(R.id.buttonSearch);
-        textView = (TextView) findViewById(R.id.text);
+        textView = (TextView) findViewById(R.id.departDate1);
+        departDate1 = (TextView) findViewById(R.id.departDate1);
+        returnDate1 = (TextView) findViewById(R.id.returnDate1);
 
-        buttonSearch.setOnClickListener(this);
+
+        returnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c = Calendar.getInstance();
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(TravelActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int mYear, int mMonth, int mDayOfMonth) {
+                        returnDate1.setText("Return Date: "+ mDayOfMonth + "/" + (mMonth + 1) + "/" + mYear);
+                    }
+                }, day, month , year);
+                dpd.show();
+
+            }
+        });
+
+        departDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c = Calendar.getInstance();
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(TravelActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int mYear, int mMonth, int mDayOfMonth) {
+                        departDate1.setText("Depart Date: "+ mDayOfMonth + "/" + (mMonth + 1) + "/" + mYear);
+                    }
+                }, day, month , year);
+                dpd.show();
+
+            }
+        });
 
     }
 

fed1550412ace005dfc0e67352eab16810c566ea
Alex Goose <sc17ag@leeds.ac.uk>
2019-11-16 00:00:31 +0000

    Help me

--- TravelabApp/app/src/main/java/com/example/travelabapp/MessageActivity.java
+++ TravelabApp/app/src/main/java/com/example/travelabapp/MessageActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;

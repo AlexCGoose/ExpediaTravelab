885623178ddd5f8b84891e837ca550ecf3482926
Alex Goose <sc17ag@leeds.ac.uk>
2019-11-16 03:31:24 +0000

    Group buttons

--- TravelabApp/app/src/main/java/com/example/travelabapp/GroupActivity.java
+++ TravelabApp/app/src/main/java/com/example/travelabapp/GroupActivity.java
@@ -14,6 +14,11 @@ import com.google.firebase.auth.FirebaseAuth;
 public class GroupActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageButton buttonMessage;
+    private Button budgetButton;
+    private Button locationButton;
+    private Button flightsButton;
+    private Button accomidationButton;
+    private Button eventsButton;
     private FirebaseAuth firebaseAuth;
 
     @Override
@@ -24,6 +29,21 @@ public class GroupActivity extends AppCompatActivity implements View.OnClickList
         buttonMessage = (ImageButton) findViewById(R.id.messageButton);
         buttonMessage.setOnClickListener(this);
 
+        budgetButton = (Button) findViewById(R.id.budgetButton);
+        budgetButton.setOnClickListener(this);
+
+        locationButton = (Button) findViewById(R.id.locationButton);
+        locationButton.setOnClickListener(this);
+
+        flightsButton = (Button) findViewById(R.id.flightsButton);
+        flightsButton.setOnClickListener(this);
+
+        accomidationButton = (Button) findViewById(R.id.accomidationButton);
+        accomidationButton.setOnClickListener(this);
+
+        eventsButton = (Button) findViewById(R.id.eventsButton);
+        eventsButton.setOnClickListener(this);
+
         firebaseAuth = firebaseAuth.getInstance();
     }
 
@@ -32,6 +52,31 @@ public class GroupActivity extends AppCompatActivity implements View.OnClickList
         if(view == buttonMessage) {
             finish();
             startActivity(new Intent(this, MessageActivity.class));;
+        }
+
+        if(view == budgetButton) {
+            finish();
+            startActivity(new Intent(this, BudgetActivity.class));;
+        }
+
+        if(view == locationButton) {
+            finish();
+            startActivity(new Intent(this, LocationActivity.class));;
+        }
+
+        if(view == flightsButton) {
+            finish();
+            startActivity(new Intent(this, FlightsActivity.class));;
+        }
+
+        if(view == accomidationButton) {
+            finish();
+            startActivity(new Intent(this, AccomidationActivity.class));;
+        }
+
+        if(view == eventsButton) {
+            finish();
+            startActivity(new Intent(this, EventsActivity.class));;
         }
     }
 }

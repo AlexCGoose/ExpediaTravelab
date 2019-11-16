5b1228140e7df8da8abedf095c90f5372b10418b
Infracanine <samueltaylorhansen@gmail.com>
2019-11-16 00:40:33 +0000

    modified:   TravelabApp/app/build.gradle 	modified:   TravelabApp/app/src/main/java/com/example/travelabapp/MainActivity.java 	modified:   TravelabApp/app/src/main/res/layout/activity_main.xml

--- TravelabApp/app/src/main/java/com/example/travelabapp/LoginActivity.java
+++ TravelabApp/app/src/main/java/com/example/travelabapp/LoginActivity.java
@@ -80,7 +80,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
         if(view == textViewSignUp) {
             finish();
-            startActivity(new Intent(this, RegisterActivity.class));
+            startActivity(new Intent(this, MainActivity.class));
         }
     }
 }
--- TravelabApp/app/src/main/java/com/example/travelabapp/MainActivity.java
+++ TravelabApp/app/src/main/java/com/example/travelabapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.travelabapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+    Button newbtn;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addButton();
+            }
+        });
     }
+        public void addButton(){
+            LinearLayout layout = (LinearLayout) findViewById(R.id.rootLayout);
+            newbtn = new Button(this);
+            newbtn.setText("Group Name");
+            layout.addView(newbtn);
+        }
 }
